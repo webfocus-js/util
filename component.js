1441bc870687ba407f8cb44858609ff64ba4cefb
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2021-06-14 15:42:00 +0100

    Fixed unwanted access

--- component.js
+++ component.js
@@ -1,4 +1,4 @@
 
 let component = module.exports = require('@webfocus/component')('util','Framework utilities like pagination and submit json.')
 component.hidden = true;
-component.staticApp.prototype = require('express').static(require('path').join(__dirname, 'cli'))
+component.staticApp = require('express').static(require('path').join(__dirname, 'cli'))
