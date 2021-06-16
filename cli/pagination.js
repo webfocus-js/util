7eebb76e98bd8dabfa2cfd8df57ece6afb1e0feb
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2021-06-16 17:26:09 +0100

    Enable distinguish multiple data-pagination

--- cli/pagination.js
+++ cli/pagination.js
@@ -6,6 +6,7 @@ window.addEventListener('load', () => {
     
     let paginationElements = document.querySelectorAll("[data-pagination-url]")
     paginationElements.forEach(n => {
+        let id = paginationElements.length > 1 ? n.id : ""; // distinguish multiple pagination using element's id
         let url = n.dataset.paginationUrl;
         
         let start = parseInt(n.dataset.paginationStart) || 0
@@ -27,15 +28,15 @@ window.addEventListener('load', () => {
             currentSearch.set('start', nextSearch.get('start'))
             currentSearch.set('end', nextSearch.get('end'))
             let nextSearchUrl = '?'+currentSearch.toString() 
-            document.querySelectorAll("[data-pagination-error]").forEach( elem => elem.textContent = error )
-            document.querySelectorAll("[data-pagination-length]").forEach( elem => elem.textContent = length )
-            document.querySelectorAll("[data-pagination-start]").forEach( elem => elem.textContent = start )
-            document.querySelectorAll("[data-pagination-end]").forEach( elem => elem.textContent = end )
-            document.querySelectorAll("[data-pagination-previous]").forEach( a => {
+            document.querySelectorAll(`[data-pagination-error='${id}']`).forEach( elem => elem.textContent = error )
+            document.querySelectorAll(`[data-pagination-length='${id}']`).forEach( elem => elem.textContent = length )
+            document.querySelectorAll(`[data-pagination-start='${id}']`).forEach( elem => elem.textContent = start )
+            document.querySelectorAll(`[data-pagination-end='${id}']`).forEach( elem => elem.textContent = end )
+            document.querySelectorAll(`[data-pagination-previous='${id}']`).forEach( a => {
                 a.textContent = "Previous";
                 a.href = previousSearchUrl
             });
-            document.querySelectorAll("[data-pagination-next]").forEach( a => {
+            document.querySelectorAll(`[data-pagination-next='${id}']`).forEach( a => {
                 a.textContent = "Next";
                 a.href = nextSearchUrl
             });
@@ -60,7 +61,7 @@ window.addEventListener('load', () => {
                 })
                 n.appendChild(templateInstance)
                 
-                let custom = new CustomEvent("PaginationAddedEntry", { detail: { element: n.lastChild, data: entry} })
+                let custom = new CustomEvent("PaginationAddedEntry", { detail: { element: n.lastChild, data: entry, id: id, url: url} })
                 window.dispatchEvent(custom)
                 
             })
